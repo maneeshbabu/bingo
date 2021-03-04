ff3626700db0dda5c027408eb36808abbf1c583f
Maneesh Babu M <maneesh@amagi.com>
2021-03-04 18:50:31 +0530

    Install quasar

---
+++ frontend/src/quasar.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+import './styles/quasar.sass'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  }
+ })
