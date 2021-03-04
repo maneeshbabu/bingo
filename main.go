cfce4bee965d9f86e85e78cf6cfa8ae5e20777e3
Maneesh Babu M <maneesh@amagi.com>
2021-03-04 17:31:39 +0530

    Change port

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
